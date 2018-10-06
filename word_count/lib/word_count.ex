f9cb9b6a81db36bce04957130c75dfa204440f94
Martin Wiso <martin@wiso.cz>
2018-10-06 15:17:44 +0200

    Add final solution

--- word_count/lib/word_count.ex
+++ word_count/lib/word_count.ex
@@ -11,5 +11,26 @@ defmodule Pyladies.WordCount do
   """
   @spec count(String.t()) :: map()
   def count(sentence) do
+    sentence
+    |> String.downcase()
+    |> remove_punctuation()
+    |> split_sentence()
+    |> summarize()
+  end
+
+  defp remove_punctuation(string) do
+    String.replace(string, @non_words, " ")
+  end
+
+  defp split_sentence(sentence) do
+    List.flatten(String.split(sentence))
+  end
+
+  defp summarize(words) do
+    Enum.reduce(words, %{}, &add_count/2)
+  end
+
+  defp add_count(word, counts) do
+    Map.update(counts, word, 1, &(&1 + 1))
   end
 end
